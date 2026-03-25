fa0a34fd56c76b74aad9165ac302bcb9aae94a4e
perezzil <perezzil@OA-S141-10>
2026-03-25 11:41:42 -0400

    made the project, got a stand in to animate

---
+++ Assets/scripts/system/pitch.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class pitch : MonoBehaviour
+{
+    public Animator Animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
+
