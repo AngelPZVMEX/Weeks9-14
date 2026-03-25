d751a4f58b3ff1726224003d74ae452eb6f3fb3d
perezzil <perezzil@OA-S141-10>
2026-03-25 09:41:01 -0400

    added sounds to footsteps in animation

---
+++ Assets/scripts/week10/knigstep.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class knigstep : MonoBehaviour
+{
+    public AudioSource SFX;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void footstep()
+    {
+        SFX.Play();
+    }
+}
