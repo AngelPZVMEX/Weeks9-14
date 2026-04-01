41beceb80731c1fe55526555806b271d1ff78f67
perezzil <perezzil@OA-S141-10>
2026-04-01 10:33:41 -0400

    made the knight moce and had the camera track him and shake on footsteps

--- Assets/scripts/week11/gymstep.cs
+++ Assets/scripts/week11/gymstep.cs
@@ -1,7 +1,9 @@
 
 
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
+
 
 public class gymstep : MonoBehaviour
 {
@@ -10,17 +12,26 @@ public class gymstep : MonoBehaviour
     public List<AudioClip> sounds;
     public AudioSource source;
     public int rand;
-    
+    Vector2 knightpos;
+    float speed = .02f;
+    public CinemachineImpulseSource impulsesource;
+
 
     void Start()
     {
-        
+        knightpos.x = 0;
+        knightpos.y = -3;
+        transform.position = knightpos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < 32)
+        {
+            knightpos.x += speed;
+            transform.position = knightpos;
+        }
     }
 
     public void footsteps()
@@ -28,6 +39,6 @@ public class gymstep : MonoBehaviour
         rand = (Random.Range(0, sounds.Count));
 
         source.PlayOneShot(sounds[rand]);
-
+        impulsesource.GenerateImpulse();
     }
 }
