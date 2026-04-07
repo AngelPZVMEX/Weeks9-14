5872e43d4595ae73ab538f4ec6f0beeef189853d
perezzil <perezzil@OA-S143-08>
2026-04-07 15:03:36 -0400

    made a baseball prefab, made a script for ball to animate

---
+++ Assets/scripts/system/bethrown.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class bethrown : MonoBehaviour
+{
+    public Component glove;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
